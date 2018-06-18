822d438de1dfd8207be6d2213b080bd8f1cf0f56
senamit2708 <senamit2708@gmail.com>
2018-06-18 21:59:30 +0530

    Google services added in gradle and json from firebase

---
+++ app/src/main/java/com/example/senamit/stationarynavigationtwo/MainActivityFirst.java
@@ -0,0 +1,13 @@
+package com.example.senamit.stationarynavigationtwo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityFirst extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_first);
+    }
+}
