c37a8d64733ac56f6c1806d0b5273bcfd11d34bf
senamit2708 <senamit2708@gmail.com>
2018-07-12 22:06:41 +0530

    ImageUpload in firebase storage

--- app/src/main/java/com/example/senamit/stationarynavigationtwo/ProductImageUpload.java
+++ app/src/main/java/com/example/senamit/stationarynavigationtwo/ProductImageUpload.java
@@ -1,22 +1,32 @@
 package com.example.senamit.stationarynavigationtwo;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.flags.IFlagProvider;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.UUID;
 
 public class ProductImageUpload extends AppCompatActivity implements View.OnClickListener {
 
@@ -28,7 +38,7 @@ public class ProductImageUpload extends AppCompatActivity implements View.OnClic
     ImageView mImage;
     //private Uri filePath;
     private Uri uri;
-    private StorageReference mRef;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +51,7 @@ public class ProductImageUpload extends AppCompatActivity implements View.OnClic
         mBtnSelect.setOnClickListener(this);
         mBtnUpload.setOnClickListener(this);
 
-        mRef = FirebaseStorage.getInstance().getReference();
+        storageReference = FirebaseStorage.getInstance().getReference();
 
 
     }
@@ -64,6 +74,58 @@ public class ProductImageUpload extends AppCompatActivity implements View.OnClic
     }
 
     private void uploadImage() {
+        if (uri!= null){
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("uploading....");
+            progressDialog.show();
+            final StorageReference ref = storageReference.child("product/images/"+ UUID.randomUUID().toString());
+            ref.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    progressDialog.dismiss();
+                    Toast.makeText(ProductImageUpload.this, "Upload successful", Toast.LENGTH_SHORT).show();
+                  getUrlOfImage();
+
+                }
+
+                private void getUrlOfImage() {
+                    ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Log.i(TAG, "the download link is "+uri);
+                        }
+                    });
+                }
+            })
+
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(ProductImageUpload.this,
+                                    "Upload Unsuccessful", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+
+                        }
+                    })
+            .continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                        @Override
+                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                            // Forward any exceptions
+                            if (!task.isSuccessful()) {
+                                throw task.getException();
+                            }
+                            Log.i(TAG, "the download url is "+ref.getDownloadUrl());
+                            return ref.getDownloadUrl();
+                        }
+                    });
+        }
     }
 
     private void chooseImage() {
