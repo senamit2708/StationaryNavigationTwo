d477b945eebce4ebdd7d3a1e019b182427202bae
senamit2708 <senamit2708@gmail.com>
2018-07-16 21:33:08 +0530

    canary android-studio

---
+++ app/src/main/java/com/example/senamit/stationarynavigationtwo/Canary/CanaryMainActivity.java
@@ -0,0 +1,17 @@
+package com.example.senamit.stationarynavigationtwo.Canary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.senamit.stationarynavigationtwo.R;
+
+public class CanaryMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canary_main);
+
+
+    }
+}
